2f9a7570dddb7bbdc3cc84b6e677fedc401083ef
SobshDev <s.sobsh@proton.me>
2025-12-03 12:52:34 +0100

    fix(cli): improve argument handling and default port logic in parse_receive

--- src/cli/parse.c
+++ src/cli/parse.c
@@ -1,13 +1,16 @@
 #include <string.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include "cli.h"
 #include "../const.h"
+#include "../common/dbgprintf.h"
 
 
 static CLI_T parse_send(int ac, char *av[])
 {
     CLI_T cli = {};
 
+    (void)ac;(void)av;
     cli.operation_type = 1;
 
     return cli;
@@ -17,12 +20,28 @@ static CLI_T parse_receive(int ac, char *av[])
 {
     CLI_T cli = {};
 
-    cli.operation_type = 2;
-
-    if (ac == 2)
+    if (ac == 2) {
         cli.port = DEFAULT_PORT;
-    else {
+        dbgprintf("Port not found, using default port");
     }
+    if (ac > 4) {
+        dbgprintf("Too many arguments");
+        return cli;
+    }
+    if (ac == 4) {
+        if (strcmp(av[2], "--port") != 0) {
+            dbgprintf("Invalid parametter, use chunknet -h to get help");
+            return cli;
+        }
+        int port = atoi(av[3]);
+        if (port <= 0 || port > 65535) {
+            dbgprintf("Invalid port, must be between 0 and 65535");
+            return cli;
+        } else {
+            cli.port = port;
+        }
+    }
+    cli.operation_type = 2;
     return cli;
 }
 
