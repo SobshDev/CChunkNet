4bb5505dad534dab28e19bf4528744b122565093
SobshDev <s.sobsh@proton.me>
2025-12-03 11:59:41 +0100

    feat(net): add core structure definitions and main signature

---
+++ src/cnet.h
@@ -0,0 +1,12 @@
+#ifndef CNET_H
+#define CNET_H
+
+typedef struct CNET_S {
+    float protocol_version;
+    unsigned short port;
+    unsigned int chunk_size;
+    unsigned int max_filename_len;
+    unsigned int magic_number;
+} CNET_T;
+
+#endif // CNET_H
---
+++ src/const.h
@@ -0,0 +1,15 @@
+#ifndef CONST_H
+#define CONST_H
+
+#define DEFAULT_PORT=4200
+
+// Chunk size in bytes
+#define CHUNK_SIZE=262144 // 256 KB
+
+#define MAX_FILENAME_LEN=4096
+
+#define PROTOCOL_VERSION=1.0
+
+#define MAGIC_NUMBER=0x434E4554 // "CNET"
+
+#endif // CONST_H
